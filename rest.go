aadf18d32d8ec71ace551f460735c3bb7999c62c
Sean Zheng <blackhorseya@gmail.com>
2024-04-20 19:07:34 +0800

    chore: consolidate package naming conventions
    - Rename the package from `fugle_marketdata_go` to `fuglemarketdatago` in `rest.go` and `websocket.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- rest.go
+++ rest.go
@@ -1,4 +1,4 @@
-package fugle_marketdata_go
+package fuglemarketdatago
 
 // RestClient is a struct that represents the rest client.
 type RestClient struct {
--- websocket.go
+++ websocket.go
@@ -1,4 +1,4 @@
-package fugle_marketdata_go
+package fuglemarketdatago
 
 // WebSocketClient is a struct that represents the websocket client.
 type WebSocketClient struct {
