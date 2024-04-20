e51ccec846fc650635d1e50192e60da2a8f4418f
Sean Zheng <blackhorseya@gmail.com>
2024-04-20 19:16:42 +0800

    feat: refactor RestClient struct and options
    - Add a defaultRestClientEndpoint constant
    - Define a RestClientOption struct with Endpoint and APIKey fields
    - Add a RestClient struct with an option field
    - Implement a NewRestClient function to create a new rest client
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- rest.go
+++ rest.go
@@ -1,5 +1,27 @@
 package fugle_marketdata
 
+const defaultRestClientEndpoint = "https://api.fugle.tw/marketdata/v1.0/stock"
+
+// RestClientOption is a struct that represents the rest client option.
+type RestClientOption struct {
+	Endpoint string `json:"endpoint"`
+	APIKey   string `json:"apiKey"`
+}
+
 // RestClient is a struct that represents the rest client.
 type RestClient struct {
+	option *RestClientOption
+}
+
+// NewRestClient is a function used to create a new rest client.
+func NewRestClient(option *RestClientOption) (*RestClient, error) {
+	if option.Endpoint == "" {
+		option.Endpoint = defaultRestClientEndpoint
+	}
+
+	// todo: 2024/4/20|sean|implement the rest client
+
+	return &RestClient{
+		option: option,
+	}, nil
 }
