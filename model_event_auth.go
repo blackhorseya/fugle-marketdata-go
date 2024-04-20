f9fbbb2fdf819b65c5dbf384188f0052fa3477f6
Sean Zheng <blackhorseya@gmail.com>
2024-04-20 19:47:14 +0800

    feat: enhance authentication event data structure
    - Add `APIKey` field to `Data` struct in `AuthEventRequest`
    - Add `Message` field to `Data` struct in `AuthEventResponse`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- model_event_auth.go
+++ model_event_auth.go
@@ -3,6 +3,9 @@ package fugle_marketdata
 // AuthEventRequest is a struct that represents the auth event request.
 type AuthEventRequest struct {
 	Event string `json:"event"`
+	Data  struct {
+		APIKey string `json:"apikey"`
+	} `json:"data"`
 }
 
 func (e *AuthEventRequest) GetEvent() string {
@@ -12,6 +15,9 @@ func (e *AuthEventRequest) GetEvent() string {
 // AuthEventResponse is a struct that represents the auth event response.
 type AuthEventResponse struct {
 	Event string `json:"event"`
+	Data  struct {
+		Message string `json:"message"`
+	} `json:"data"`
 }
 
 func (e *AuthEventResponse) GetEvent() string {
