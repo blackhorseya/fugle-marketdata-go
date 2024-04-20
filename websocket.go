8d6b04423ee2ddb5b85883720dc2cfff114df368
Sean Zheng <blackhorseya@gmail.com>
2024-04-20 19:34:04 +0800

    feat: create WebSocket client with configurable options
    - Add a new struct `WebSocketClientOption` with `Endpoint` and `APIKey` fields
    - Add `Dial` and `DialWithContext` functions to create a new websocket client
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- websocket.go
+++ websocket.go
@@ -2,11 +2,36 @@ package fugle_marketdata
 
 import (
 	"github.com/gorilla/websocket"
+	"golang.org/x/net/context"
 )
 
 const defaultWebSocketClientEndpoint = "wss://api.fugle.tw/marketdata/v1.0/stock/streaming"
 
+// WebSocketClientOption is a struct that represents the websocket client option.
+type WebSocketClientOption struct {
+	Endpoint string `json:"endpoint"`
+	APIKey   string `json:"apiKey"`
+}
+
 // WebSocketClient is a struct that represents the websocket client.
 type WebSocketClient struct {
 	conn *websocket.Conn
+}
+
+// Dial is a function used to create a new websocket client.
+func Dial(option *WebSocketClientOption) (*WebSocketClient, error) {
+	return DialWithContext(context.Background(), option)
+}
+
+// DialWithContext is a function used to create a new websocket client.
+func DialWithContext(ctx context.Context, option *WebSocketClientOption) (*WebSocketClient, error) {
+	conn, resp, err := websocket.DefaultDialer.DialContext(ctx, option.Endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return &WebSocketClient{
+		conn: conn,
+	}, nil
 }
