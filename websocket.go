d52db86368ecf25dd1bd4af067ba415d37d5e023
Sean Zheng <blackhorseya@gmail.com>
2024-04-20 19:29:00 +0800

    feat: consolidate WebSocket client implementation
    - Add a defaultWebSocketClientEndpoint constant
    - Define the WebSocketClient struct with a conn field
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- websocket.go
+++ websocket.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+const defaultWebSocketClientEndpoint = "wss://api.fugle.tw/marketdata/v1.0/stock/streaming"
+
 // WebSocketClient is a struct that represents the websocket client.
 type WebSocketClient struct {
 	conn *websocket.Conn
