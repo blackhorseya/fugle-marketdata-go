caa2633b41d2523ff76475ac0ea24e6f78623abd
Sean Zheng <blackhorseya@gmail.com>
2024-04-20 19:20:17 +0800

    test: refactor REST client implementation and testing
    - Add imports for `fmt` and `net/url` in `rest.go`
    - Implement a test for `NewRestClient` in `rest_test.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- rest.go
+++ rest.go
@@ -1,5 +1,10 @@
 package fugle_marketdata
 
+import (
+	"fmt"
+	"net/url"
+)
+
 const defaultRestClientEndpoint = "https://api.fugle.tw/marketdata/v1.0/stock"
 
 // RestClientOption is a struct that represents the rest client option.
@@ -17,6 +22,11 @@ type RestClient struct {
 func NewRestClient(option *RestClientOption) (*RestClient, error) {
 	if option.Endpoint == "" {
 		option.Endpoint = defaultRestClientEndpoint
+	}
+
+	_, err := url.ParseRequestURI(option.Endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("parse rest client endpoint failed: %w", err)
 	}
 
 	// todo: 2024/4/20|sean|implement the rest client
---
+++ rest_test.go
@@ -0,0 +1,38 @@
+package fugle_marketdata
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewRestClient(t *testing.T) {
+	type args struct {
+		option *RestClientOption
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *RestClient
+		wantErr bool
+	}{
+		{
+			name:    "invalid url then error",
+			args:    args{option: &RestClientOption{Endpoint: "invalid"}},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewRestClient(tt.args.option)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewRestClient() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			t.Logf("err: %v", err)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewRestClient() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
