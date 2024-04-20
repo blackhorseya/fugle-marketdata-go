236e8fd2ff01c77048b4f1fdb0e5e7c532367a4c
Sean Zheng <blackhorseya@gmail.com>
2024-04-20 19:23:57 +0800

    chore: integrate github.com/gorilla/websocket into project
    - Add `github.com/gorilla/websocket` as a required module in `go.mod`
    - Add import statement for `github.com/gorilla/websocket` in `websocket.go`
    - Initialize `conn` field in `WebSocketClient` struct in `websocket.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- websocket.go
+++ websocket.go
@@ -1,5 +1,10 @@
 package fugle_marketdata
 
+import (
+	"github.com/gorilla/websocket"
+)
+
 // WebSocketClient is a struct that represents the websocket client.
 type WebSocketClient struct {
+	conn *websocket.Conn
 }
